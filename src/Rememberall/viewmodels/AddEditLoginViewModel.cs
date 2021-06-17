0207f60dcad467bbb7abccc6af5ca47d04df4729
Jarad Gray <jarad.h.gray@gmail.com>
2021-06-17 14:48:28 -0500

    create placeholder AddEditLogin view for verification

---
+++ src/Rememberall/viewmodels/AddEditLoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rememberall
+{
+    public class AddEditLoginViewModel : BaseINPC
+    {
+        private Login _login;
+        public Login Login
+        {
+            get { return _login; }
+            set
+            {
+                if (value == _login) return;
+                _login = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
