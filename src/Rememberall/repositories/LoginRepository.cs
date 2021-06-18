787d250feffaa094f5ffb2d5f0e0b4e5bb2ee317
Jarad Gray <jarad.h.gray@gmail.com>
2021-06-18 15:49:35 -0500

    LoginRepository gets data from database

--- src/Rememberall/repositories/LoginRepository.cs
+++ src/Rememberall/repositories/LoginRepository.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace Rememberall
 {
@@ -11,65 +14,36 @@ namespace Rememberall
         private const string CONNECTION_STRING = @"Data Source=.\data\dummy.db;Version=3;"; // the connection string for the database we'll connect to
 
         /// <summary>
-        /// Returns a List containing all Login objects in the database whose
-        /// Folder property matches the given Folder, or all Logins if the
-        /// given Folder.FolderType property is AllFolders
+        /// Returns a List containing all Login objects in the database's LoginTable
+        /// whose FolderName property matches the given Folder's Name property, or
+        /// all Logins if the given Folder.FolderType property is AllFolders
         /// </summary>
         /// <param name="folder"></param>
         /// <returns></returns>
         public static List<Login> GetLoginsInFolder(Folder folder)
         {
-            var result = new List<Login>();
-            switch (folder.FolderType)
+            if (folder.FolderType == Folder.Type.AllItems) return GetAllLogins();
+
+            using (IDbConnection connection = new SQLiteConnection(CONNECTION_STRING))
             {
-                case Folder.Type.Folder:
-                    // TODO get Logins from database
-                    result.Add(new Login()
-                    {
-                        Title = "Login 1 in folder",
-                        Username = "the username"
-                    });
-                    result.Add(new Login()
-                    {
-                        Title = "Login 2 in folder",
-                        Username = "Mandrake"
-                    });
-                    result.Add(new Login()
-                    {
-                        Title = "Login 3 in folder",
-                        Username = "Leviathan"
-                    });
-                    break;
-                case Folder.Type.AllItems:
-                    // TODO get all Logins from database
-                    result.Add(new Login()
-                    {
-                        Title = "Login 1 in folder",
-                        Username = "the username"
-                    });
-                    result.Add(new Login()
-                    {
-                        Title = "Login 2 in folder",
-                        Username = "Mandrake"
-                    });
-                    result.Add(new Login()
-                    {
-                        Title = "Login 3 in folder",
-                        Username = "Leviathan"
-                    });
-                    result.Add(new Login()
-                    {
-                        Title = "Login 4 in different folder",
-                        Username = "Hanzel"
-                    });
-                    result.Add(new Login()
-                    {
-                        Title = "Login 5 in different folder",
-                        Username = "Gretel"
-                    });
-                    break;
+                string query = "SELECT * FROM LoginTable WHERE FolderName = '" + folder.Name + "'";
+                var output = connection.Query<Login>(query, new DynamicParameters());
+                return new List<Login>(output);
             }
-            return result;
+        }
+
+        /// <summary>
+        /// Returns a List of Login objects representing each record in the database's LoginTable
+        /// </summary>
+        /// <returns></returns>
+        private static List<Login> GetAllLogins()
+        {
+            using (IDbConnection connection = new SQLiteConnection(CONNECTION_STRING))
+            {
+                string query = "SELECT * FROM LoginTable";
+                var output = connection.Query<Login>(query, new DynamicParameters());
+                return new List<Login>(output);
+            }
         }
     }
 }
