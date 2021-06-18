d356d7b884233b01850d8f45c317eeabb1c1c100
Jarad Gray <jarad.h.gray@gmail.com>
2021-06-18 15:13:57 -0500

    update Login class properties to match database better

--- src/Rememberall/models/Login.cs
+++ src/Rememberall/models/Login.cs
@@ -8,16 +8,17 @@ namespace Rememberall
 {
     public class Login
     {
-        // TODO image for website favicon?
+
+        public long TicksCreated { get; private set; }
+        public long TicksModified { get; set; }
+        public string FolderName { get; set; }
+        public string FaviconPath { get; set; }
         public string Title { get; set; }
         public string Website { get; set; }
         public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Note { get; set; }
-        public Folder Folder { get; set; }
-        public long TicksCreated { get; private set; }
-        public long TicksModified { get; set; }
         public bool IsFavorite { get; set; }
 
         public Login()
